12995583cf01828a99e1367cb6619ab318f16b3b
Dmitriy Makarenko <dmakarenko@gmail.com>
2012-04-14 11:59:54 +0300

    change default packege to "forum"

---
+++ Server/src/forum/Server.java
@@ -0,0 +1,37 @@
+package forum;
+import java.io.IOException;
+
+/**
+ * Server class represents entry point to run program
+ * @author Andrey
+ * @version 1.0
+ */
+public class Server {
+	/**
+	 * Main method
+	 * @param args List of arguments passed from command line
+	 */
+	public static void main(String[] args) {
+		// Set default port
+		int port = 1234;
+		
+		// Consider arguments
+		if (args.length == 1) {
+			port = Integer.parseInt(args[0]);
+		}
+		else if (args.length > 1) {
+			System.out.println("Usage: java  -jar  Server.jar [listener port]");
+			return;
+		}
+		
+		
+		Forum f = null;
+		try {
+			f = new Forum(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		f.run();
+	}
+
+}
